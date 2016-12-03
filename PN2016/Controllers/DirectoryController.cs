a5c113b463e10f4de729571f14329eef2873fb3d
Karuppiah Ganesan <karuppiah@gmail.com>
2016-12-03 11:03:55 -0500

    Added DirectoryController

---
+++ PN2016/Controllers/DirectoryController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace PN2016.Controllers
+{
+    public class DirectoryController : Controller
+    {
+        // GET: Directory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Directory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Directory/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
