bb9cdc809d713aa93daa5d53a8abe72b2c83ebdd
Karuppiah Ganesan <karuppiah@gmail.com>
2016-12-03 10:11:23 -0500

    Added MVC+API Project from Visual Studio

---
+++ PN2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PN2016.Startup))]
+namespace PN2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
